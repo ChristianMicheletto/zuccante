a3878ee95709faac4f5180c897e28df86c79953b
Andrea Morettin <divino.marchese@gmail.com>
2015-09-30 16:54:44 +0200

    printStackTrace() in action

---
+++ java/others/TestPST.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+
+public class TestPST {
+    public static void main(String[] args){
+        try {
+            throw new NullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+        
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
