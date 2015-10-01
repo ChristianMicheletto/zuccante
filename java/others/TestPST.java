0f65f11db52dd9f9dff7f5f9c3227afcb4390379
Andrea Morettin <divino.marchese@gmail.com>
2015-10-01 21:30:57 +0200

    ripasso

--- java/others/TestPST.java
+++ java/others/TestPST.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 
-
 public class TestPST {
     public static void main(String[] args){
         try {
---
+++ java/others/TestRandom.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class TestRandom {
+    
+    public static void main(String[] args) {
+        
+        Random r; 
+        long seed;
+        
+        System.out.println("sequenza I");
+        seed = 10;
+        r = new Random(seed);
+        for(int i = 0; i < 10; i++) {
+            System.out.print(r.nextInt(60)+" ");
+        }
+        System.out.print("\n\n");
+        
+        System.out.println("sequenza II");
+        seed = 10;
+        r = new Random(seed);
+        for(int i = 0; i < 10; i++) {
+            System.out.print(r.nextInt(60)+" ");
+        }
+        System.out.print("\n\n");
+        
+        System.out.println("sequenza III");
+        seed = 33;
+        r = new Random(seed);
+        for(int i = 0; i < 10; i++) {
+            System.out.print(r.nextInt(60)+" ");
+        }
+        System.out.print("\n\n");
+    }
+}
