6d6fa349bf15718ee15203ec3ec84be5061b6449
Andrea Morettin <divino.marchese@gmail.com>
2015-10-04 11:03:14 +0200

    add DB connection and counter dir in thread

---
+++ java/thread/counter/TestCounter.java
@@ -0,0 +1,32 @@
+public class TestCounter {
+    
+    // un'unica risorsa cui accederanno i 2 thread
+    static final Counter c = new Counter();
+
+    public static void main(String[] args) {
+        
+        // first thread
+        new Thread(new Runnable() {
+            public void run() { 
+                System.out.println(c.getValue()); 
+                c.increment();
+                try {
+                    // thread to sleep for 1 milliseconds
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                        e.printStackTrace();
+                }
+                c.decrement();
+            }
+        }).start();
+        
+        // second thread
+        new Thread(new Runnable() {
+            public void run() { 
+                System.out.println(c.getValue()); 
+                c.increment();
+                System.out.println(c.getValue()); 
+            }
+        }).start();
+    }
+}
