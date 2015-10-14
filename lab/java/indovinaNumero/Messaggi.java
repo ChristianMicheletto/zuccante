047f9cbaabf2a77f7180adcd613c5ae7ba17c5be
Andrea Morettin <divino.marchese@gmail.com>
2015-10-14 17:45:24 +0200

    proposte per le soluzioni di alcune esercitazioni di laboratorio

---
+++ lab/java/indovinaNumero/Messaggi.java
@@ -0,0 +1,10 @@
+public interface Messaggi {
+	static final String OK = "Bravo, hai indovinato!";
+	static final String TRY = "Inserisci un numero, sei al tentativo n. ";
+	static final String NTENT = "Tentativo numero ";
+	static final String MINORE ="Non hai indovinato, è più piccolo."; 
+	static final String MAGGIORE = "Non hai indovinato, è più grande.";
+	static final String WELCOME = "Benvenuto!";
+	static final String NOMORETRIES = "Non hai più tentativi!";
+	static final String FINISH = "Partita conclusa!";
+}
---
+++ lab/java/indovinaNumero/README.txt
@@ -0,0 +1,1 @@
+by Favaro e Pagnin 5IC
