4a8e5de5ce2e461367c40caf50b4e7a1cb6bace2
Andrea Morettin <divino.marchese@gmail.com>
2015-10-26 17:55:20 +0100

    update udp

--- java/udp/UDPServer.java
+++ java/udp/UDPServer.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 class UDPServer {
     public static void main(String args[]) throws Exception {
         DatagramSocket serverSocket = new DatagramSocket(9876);
         DatagramPacket receivePacket = null;
-        byte[] receiveData = null;
-        byte[] sendData = null;
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = new byte[1024];
         while(true) {
-            receiveData = new byte[1024];
-            sendData = new byte[1024];
+            Arrays.fill(receiveData, (byte)0); // try to remove
+            Arrays.fill(sendData, (byte)0); // try to remove
             receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
             String sentence = new String(receivePacket.getData());
