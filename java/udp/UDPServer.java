4ad8b6d2ef590b48bee1bcf8eead15f4c0dc2a34
Andrea Morettin <divino.marchese@gmail.com>
2015-10-26 16:43:51 +0100

    adding udp

---
+++ java/udp/UDPServer.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.net.*;
+
+class UDPServer {
+    public static void main(String args[]) throws Exception {
+        DatagramSocket serverSocket = new DatagramSocket(9876);
+        DatagramPacket receivePacket = null;
+        byte[] receiveData = null;
+        byte[] sendData = null;
+        while(true) {
+            receiveData = new byte[1024];
+            sendData = new byte[1024];
+            receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            String sentence = new String(receivePacket.getData());
+           
+            System.out.println("RECEIVED: " + sentence);
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            String capitalizedSentence = sentence.toUpperCase();
+            sendData = capitalizedSentence.getBytes();
+            DatagramPacket sendPacket =
+            new DatagramPacket(sendData, sendData.length, IPAddress, port);
+            serverSocket.send(sendPacket);
+        }
+    }
+}
