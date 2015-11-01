cb19becb7b2bdb0ac52b5abdbfc347ae52f1a29d
Andrea Morettin <divino.marchese@gmail.com>
2015-11-01 15:16:30 +0100

    adding http

---
+++ java/http/HTTPClient.java
@@ -0,0 +1,49 @@
+import java.net.*;
+import java.io.*;
+
+
+public class HTTPClient {
+    
+    final static int PORT = 80;
+
+    public static void main(String[] args) {
+        
+        int port = PORT;
+        
+        for (int i = 0; i < args.length; i++) {
+            try {
+                // create an URL object
+                URL url = new URL(args[i]);
+                if (url.getPort() != -1) port = url.getPort();
+                if (!(url.getProtocol().equalsIgnoreCase("http"))) {
+                    System.err.println("Sorry. I only understand http.");
+                    continue;
+                    }
+                // client
+                Socket s = new Socket(url.getHost(), port);
+                OutputStream theOutput = s.getOutputStream();
+                // no auto-flushing
+                PrintWriter pw = new PrintWriter(theOutput, false);
+                // native line endings are uncertain so add them manually
+                // \n\r ending HTTP lines
+                pw.print("GET " + url.getFile() + " HTTP/1.0\r\n");
+                pw.print("Accept: text/plain, text/html, text/*\r\n");
+                pw.print("\r\n");
+                // send message
+                pw.flush();
+                InputStreamReader isr = new InputStreamReader(s.getInputStream());
+                BufferedReader br = new BufferedReader(isr);
+                int c;
+                while ((c = br.read()) != -1) {
+                    System.out.print((char) c);
+                }
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+                System.err.println(args[i] + " is not a valid URL");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                System.err.println(ex);
+            }
+       }
+    }
+}
