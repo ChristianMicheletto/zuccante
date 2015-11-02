bf55a6cd78edde8fa12f96e54a0cc86796d808e8
Andrea Morettin <divino.marchese@gmail.com>
2015-11-02 22:09:34 +0100

    updated

--- java/udp/MulticastClient.java
+++ java/udp/MulticastClient.java
@@ -8,23 +8,21 @@ public class MulticastClient {
  
         MulticastSocket socket = new MulticastSocket(4446);
         InetAddress address = InetAddress.getByName("224.0.0.1");
-    socket.joinGroup(address);
+        socket.joinGroup(address);
  
         DatagramPacket packet;
      
-            // get a few quotes
-    for (int i = 0; i < 5; i++) {
- 
-        byte[] buf = new byte[256];
+        // get a few quotes
+        for (int i = 0; i < 5; i++) {
+            byte[] buf = new byte[256];
             packet = new DatagramPacket(buf, buf.length);
             socket.receive(packet);
  
             String received = new String(packet.getData(), 0, packet.getLength());
             System.out.println("Quote of the Moment: " + received);
-    }
+        }
  
-    socket.leaveGroup(address);
-    socket.close();
+        socket.leaveGroup(address);
+        socket.close();
     }
- 
 }
