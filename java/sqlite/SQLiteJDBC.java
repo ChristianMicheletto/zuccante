eba9debbe014f947d8e1880705ffe687ef820620
Andrea Morettin <you@example.com>
2016-10-17 21:54:19 +0200

    SQLiteJDBC.java updated

--- java/sqlite/SQLiteJDBC.java
+++ java/sqlite/SQLiteJDBC.java
@@ -1,3 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLiteJDBC {
+    
+    private static String dbName = "shop.sqlite";
+    
+    public static void main(String[] args) throws ClassNotFoundException {
+        // load the sqlite-JDBC driver using the current class loader
+        Class.forName("org.sqlite.JDBC");
+        Connection connection = null;
+        try {
+            // create a database connection (side effect create DB)
+            connection = DriverManager.getConnection("jdbc:sqlite:" + dbName);
+            /* complete it
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("DROP TABLE IF EXISTS Products");
+            statement.executeUpdate("CREATE TABLE ... ");
+            statement.executeUpdate("INSERT INTO ... ");
+            statement.executeUpdate("INSERT INTO ... ");
+            */
+      
+        } catch(SQLException e) {
+            // if the error message is "out of memory", 
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                if(connection != null)
+                connection.close();
+            } catch(SQLException e){
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
+    }
+}
+
+
+
+/* old version
+
 import java.sql.*;
 
 public class SQLiteJDBC {
@@ -17,3 +64,4 @@ public class SQLiteJDBC {
         System.out.println("Opened database successfully");
     }
 }
+*/
