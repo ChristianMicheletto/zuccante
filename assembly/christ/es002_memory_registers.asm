f0126f342b2912b49b55bd9900e2ce94db3bb0e6
Andrea Morettin <you@example.com>
2017-01-30 16:43:50 +0100

    updated

--- assembly/christ/es002_memory_registers.asm
+++ assembly/christ/es002_memory_registers.asm
@@ -2,23 +2,23 @@
 ; yasm -f elf64 -g dwarf2 <file>
 ; gcc <exe> <object> (we use main label)
 
+     segment .data
 
+a    db 4
+b    db 10
 
-segment .text                   
+     segment .text
+     global main
 
-    global  main          
-                                
 main:
 
-mov    rax, 100
-mov    eax, 100
-mov    rbx, 0xfab1fab1fab1fab1
-mov    rax, a
-
-segment .data                   ;section declaration
-
-a      dd 4
-
+     mov rax, 199
+     mov rbx, 100
+     mov rax, 0xfafafafafafafafa
+     mov ax, [a]
+     mov rax,0				; normal, no error, return value
+     ret				; return
+       
 
 
 
