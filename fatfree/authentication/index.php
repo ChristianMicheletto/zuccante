60793917ba359487ae24d0ed0bb3431935be318b
Andrea Morettin <you@example.com>
2017-05-18 22:28:38 +0200

    authentication updated

--- fatfree/authentication/index.php
+++ fatfree/authentication/index.php
@@ -1,18 +1,22 @@
 <?php
-// start server using "php -S localhost:8080 -t <path>/db01"
+// start server using "php -S localhost:8080 -t <path>/authentication"
 $f3 = require('../fatfree-master/lib/base.php');
+// debug level
 $f3->set('DEBUG',3);
-$f3->config('setup.cfg');
+// configuration
+$f3->config('setups.cfg');
+$f3->config('routes.cfg');
 // connect to databases;
 // set database
-$db=new DB\SQL(
-    'mysql:host=localhost;port=3306;dbname=fat02',
-    'fat',
-    'fat@2017'
+$db = new DB\SQL(
+    $f3->get(devdb),
+    $f3->get(devuser),
+    $f3->get(devpwd)
 );
 // set a fatfree global variable
 $f3->set('db',$db);
 
 // run server
 $f3->run();
+
 ?>
---
+++ fatfree/authentication/tmp/1rhmuqyegwrps.pw3tcs9b047x.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>My Site</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6">
+    <?php if ($logged): ?>
+      
+        <h3>User logged</h3>
+        <p>Hello, !</p>
+        <p>We display it</p> 
+      
+      <?php else: ?>
+        <h3>No User logged</h3>
+        <p>Sorry! login <a href="/login">here</a></p>.
+      
+    <?php endif; ?>
+    </div>
+    <div class="col-sm-6">
+      <h3>A text</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
