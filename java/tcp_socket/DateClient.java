f6768ac8b7050db86de37b64a3eebd3000ad003d
Andrea Morettin <you@example.com>
2017-09-23 12:25:40 +0200

    AS2017-2018 updated

--- java/tcp_socket/DateClient.java
+++ java/tcp_socket/DateClient.java
@@ -4,14 +4,19 @@ import java.io.InputStreamReader;
 import java.io.DataOutputStream;
 import java.net.Socket;
 
+/*
+ * start server using:
+ * php -S localhost:8080
+ */
+
 public class DateClient {
 
     public static void main(String[] args) throws IOException {
         String serverAddress = "localhost"; // server string
         
-        Socket s = new Socket(serverAddress, 80);
+        Socket s = new Socket(serverAddress, 8080);
         DataOutputStream dOut = new DataOutputStream(s.getOutputStream());
-        dOut.write("GET /date.php HTTP/1.0\n\n".getBytes()); // GET di HTTP
+        dOut.writeBytes("GET /index.php HTTP/1.0\n\n"); // minimal GET request
         BufferedReader input =
             new BufferedReader(new InputStreamReader(s.getInputStream()));
         String str = "DATA RECEIVED:";
